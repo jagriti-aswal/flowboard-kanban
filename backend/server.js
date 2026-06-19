85952bdf5d2172ff25ceb7561d51da9021a0f843
jagriti-aswal <jagritiaswal12@gmail.com>
2026-06-19 15:40:00 +0530

    Prepare application for deployment

--- backend/server.js
+++ backend/server.js
@@ -288,6 +288,8 @@ mongoose
     console.log(err);
   });
 
-server.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
